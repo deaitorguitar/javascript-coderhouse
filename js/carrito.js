ab51f8484d66c2b76cf74962733cf756761b34fd
Aitor Guitar <deaitorguitar@gmail.com>
2024-02-12 18:07:49 -0300

    Preentrega 3-Aitor-guitar

---
+++ js/carrito.js
@@ -0,0 +1,7 @@
+const productos = [
+    {id: 1, nombre: "brownie", precio: 10000},
+    {id: 2, nombre: "snacks", precio: 8000},
+    {id: 3, nombre: "tortas", precio: 7500},
+];
+
+console.log(productos);
--- main.js
+++ main.js
@@ -1,41 +1,69 @@
-function Suma(a, b){ 
-    return a + b;
+// function Suma(a, b){ 
+//     return a + b;
+// }
+
+// let resultado = Suma(5, 20);
+// console.log(resultado);
+
+
+// const subproductos = [ 'minibrownie', 'cafe', 'tortitas' ]
+
+// console.log( subproductos.indexOf('minibrownie'))
+// console.log( subproductos.indexOf('cafe'))
+// console.log( subproductos.indexOf('tortitas'))
+
+// let usuario = prompt("Ingresa tu nombre")
+
+// if(usuario = "Aitor"){
+//     alert("Bienvenido de vuelta Aitor")
+// }
+
+
+// const productos = [
+//     { id: 1, nombre: "Brownie", precio: 10000 },
+//     { id: 2, nombre: "Caja de snacks", precio: 7000 },
+//     { id: 3, nombre: "Tortas", precio: 12000 },
+// ];
+
+// let producto = prompt("Ingrese el producto que desea buscar")
+// let productoEncontrado = productos.find((item) => item.nombre === producto);
+
+// if(productoEncontrado) {
+//     alert(`
+//      Id: ${productoEncontrado.id}
+//      Nombre: ${productoEncontrado.nombre}
+//      Precio: ${productoEncontrado.precio}
+//     `);
+// } else {
+//     alert("Producto no disponible");
+// }
+    
+
+
+
+let cardtitle = document.getElementById("titulo-tarjeta")
+cardtitle.innerHTML = "Brownies";
+
+let cardtitle2 = document.getElementById("titulo-tarjeta2")
+cardtitle2.innerHTML = "Productos";
+
+let cardtitle3 = document.getElementById("titulo-tarjeta3")
+cardtitle3.innerHTML = "Tortas";
+
+const usuario = {
+    nombre: "Aitor",
+    Edad: 18
 }
 
-let resultado = Suma(5, 20);
-console.log(resultado);
 
+const JSONuser = JSON.stringify(usuario);
+localStorage.setItem("usuario", JSONuser);
 
-const subproductos = [ 'minibrownie', 'cafe', 'tortitas' ]
-
-console.log( subproductos.indexOf('minibrownie'))
-console.log( subproductos.indexOf('cafe'))
-console.log( subproductos.indexOf('tortitas'))
-
-let usuario = prompt("Ingresa tu nombre")
-
-if(usuario = "Aitor"){
+if(usuario.nombre = "Aitor"){
     alert("Bienvenido de vuelta Aitor")
 }
 
-
-const productos = [
-    { id: 1, nombre: "Brownie", precio: 10000 },
-    { id: 2, nombre: "Caja de snacks", precio: 7000 },
-    { id: 3, nombre: "Tortas", precio: 12000 },
-];
-
-let producto = prompt("Ingrese el producto que desea buscar")
-let productoEncontrado = productos.find((item) => item.nombre === producto);
-
-if(productoEncontrado) {
-    alert(`
-     Id: ${productoEncontrado.id}
-     Nombre: ${productoEncontrado.nombre}
-     Precio: ${productoEncontrado.precio}
-    `);
-} else {
-    alert("Producto no disponible");
-}
-    
-
+const button = document.querySelector(".boton-tarjeta")
+button.addEventListener("click", function () {
+    alert("Se agrego correctamente")
+})
